287ae031b0c1ff6825c41da7c9bcaedc3726c061
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2023-06-14 17:51:59 -0500

    al

--- Config/conexion.php
+++ Config/conexion.php
@@ -4,5 +4,5 @@ $MYSQLHOST = "containers-us-west-135.railway.app";
 $MYSQLUSER = "root";
 $MYSQLPASSWORD= "qSrM0GEuMhptsib4Ntrb";
 $MYSQLDATABASE="railway";
-
+$MYSQLPORT = "0.0.0.0";
 $conexion = mysqli_connect($MYSQLHOST,$MYSQLUSER,$MYSQLPASSWORD,$MYSQLDATABASE);
